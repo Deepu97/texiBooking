31c38bf98050ab7e2ebd1f4e4ae6199abca79362
Deepu97 <dy775829@gmail.com>
2024-07-24 22:11:22 +0530

    router hash

--- src/App.js
+++ src/App.js
@@ -10,13 +10,13 @@ import Imge from './book/Imge';
 import Navbar from './book/Navbar';
 import Ourservice from './book/Ourservice';
 import Taxi from './book/Taxi';
-import {Routes,Route} from 'react-router-dom';
+import {HashRouter,Routes,Route} from 'react-router-dom';
 
 
 function App() {
   return (
    <>
-
+<HashRouter>
 <Routes>
   <Route exact path="/" element={<><Taxi/><Backg/><Ourservice/><Gallery/><Benefit/><Navbar/></>}></Route>
 
@@ -25,6 +25,8 @@ function App() {
   <Route path="/about" element={<><Taxi/><About/><Benefit/><Navbar/></>}></Route>
   
  </Routes>
+</HashRouter>
+
   
    </>
       );
